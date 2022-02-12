e7abf0d6b557cb1b9b2100f9165c7f36d027a9c2
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-12 16:51:07 +0100

    Update prepare_to_start.sh

--- prepare_to_start.sh
+++ prepare_to_start.sh
@@ -7,5 +7,13 @@ if ! [ -x "$(command -v sudo)" ]; then
 	su - root  -c "/bin/bash $SCRIPT_DIR/.prep/set_up_sudo.sh"
 fi
 
+PYTHON_VERSION=`python3 -V | cut -d "." -f 2`
+
+# If python version is smaller than 3.9
+#   install python 3.9.8
+if [ $PYTHON_VERSION -lt 9 ]; then
+        /bin/bash $SCRIPT_DIR/.prep/install_python3_9.sh
+fi
+
 # Install python3
 /bin/bash $SCRIPT_DIR/.prep/install_dependencies.sh
