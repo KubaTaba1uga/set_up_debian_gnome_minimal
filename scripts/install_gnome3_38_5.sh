f4e11790c2673e226ed19645c139e650744620a0
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-17 18:56:11 +0100

    Update and rename install_gnome3_5.sh to install_gnome3_38_5.sh

--- scripts/install_gnome3_38_5.sh
+++ scripts/install_gnome3_38_5.sh
@@ -5,13 +5,13 @@ non_interactive="DEBIAN_FRONTEND=noninteractive"
 sudo $non_interactive apt-get install \
 libgl1-mesa-dri        \
 x11-xserver-utils      \
-gnome-core \
-gnome-session=3.38           \
+gnome-session=3.38.0           \
 gnome-applets            \
 gnome-tweak-tool        \
 gnome-icon-theme        \
-gnome-backgrounds      \
-gnome-control-center     \
-gnome-themes-standard \
+gnome-backgrounds=3.38.0      \
+gnome-control-center=3.38.6     \
+gnome-themes-standard \ 
+gnome-keyring \
 pulseaudio \
 terminator nautilus -y --no-install-recommends --fix-missing
