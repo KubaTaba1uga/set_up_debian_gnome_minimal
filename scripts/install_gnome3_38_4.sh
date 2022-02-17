2c5c340c668dbe945a7e81ec6d3390d742a1e292
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2022-02-17 19:27:16 +0100

    Update install_gnome3_38_4.sh

--- scripts/install_gnome3_38_4.sh
+++ scripts/install_gnome3_38_4.sh
@@ -5,13 +5,13 @@ non_interactive="DEBIAN_FRONTEND=noninteractive"
 sudo $non_interactive apt-get install \
 libgl1-mesa-dri        \
 x11-xserver-utils      \
-gnome-session=3.38.0           \
+gnome-session=3.30.1-2           \
 gnome-applets            \
 gnome-tweak-tool        \
 gnome-icon-theme        \
-gnome-backgrounds=3.38.0      \
-gnome-control-center=3.38.6     \
+gnome-backgrounds      \
+gnome-control-center     \
 gnome-themes-standard \ 
 gnome-keyring \
 pulseaudio \
-terminator nautilus -y --no-install-recommends --fix-missing
+terminator nautilus -y --fix-missing
