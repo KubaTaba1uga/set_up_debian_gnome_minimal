2b0a551e63474c023c187190318e41f61ca2e1df
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-07 18:43:42 +0200

    update scripts

--- .prep/install_dependencies.sh
+++ .prep/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install python3-pip libffi-dev build-essential python3-poetry -y  --no-install-recommends && \
+sudo apt-get install git python3-pip libffi-dev build-essential python3-poetry -y  --no-install-recommends && \
 python3 -m poetry install --no-dev
--- scripts/install_vertical_workspaces_extension.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
- wget https://github.com/G-dH/vertical-workspaces/releases/download/v28/vertical-workspaces@G-dH.github.com.zip
-
-gnome-extensions install --force vertical-workspaces@G-dH.github.com.zip
-
-gnome-extensions enable vertical-workspaces
-
-rm vertical-workspaces@G-dH.github.com.zip
