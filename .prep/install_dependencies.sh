caa4affe65e82a928182aa1100189d59be15dd57
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-07 14:46:33 +0200

    Update install_dependencies.sh

--- .prep/install_dependencies.sh
+++ .prep/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install python3-pip libffi-dev build-essential  -y  --no-install-recommends && \
-python3 -m pip install --upgrade pip setuptools wheel && \
-python3 -m pip install poetry && \
+sudo apt-get install python3-pip libffi-dev build-essential python3-poetry -y  --no-install-recommends && \
 python3 -m poetry install --no-dev
