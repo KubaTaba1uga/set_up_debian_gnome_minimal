af4c7ddafd157f6132590d5f99cec51cd34ec9c5
KubaTaba1uga <jakub@buczynski.net.pl>
2023-07-09 12:14:43 +0200

    hide zip file

--- scripts/install_vertical_workspaces_extension_2.sh
+++ scripts/install_vertical_workspaces_extension_2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 EXTENSION_UUID=vertical-workspaces@G-dH.github.com 
+EXTENSION_FILE=scripts/.vertical-workspaces-extension.zip
 
 sudo apt-get install --no-install-recommends -y gnome-shell-extensions
 
-gnome-extensions install --force vertical-workspaces-extension.zip
+gnome-extensions install --force $EXTENSION_FILE
 
 gnome-extensions enable $EXTENSION_UUID
 
-rm vertical-workspaces-extension.zip
+rm $EXTENSION_FILE
 
